998ba8c9999685eaef1de2766821154033ed534a
davialmeida135 <davi.almeida.135@ufrn.edu.br>
2024-06-05 23:24:52 -0300

    start

---
+++ project_manager/app/db.py
@@ -0,0 +1,34 @@
+import click
+import mysql.connector
+import os
+
+from flask import current_app, g
+from flask.cli import with_appcontext
+from flask import Flask,render_template, request
+from flask_mysqldb import MySQL
+ 
+def get_db():
+    if "db" not in g or not g.db.is_connected():
+        g.db = mysql.connector.connect(
+            host=current_app.config["DB_HOST"],
+            user=current_app.config["DB_USERNAME"],
+            password=current_app.config["DB_PASSWORD"],
+            database=current_app.config["DB_DATABASE"],
+
+            )
+        pass
+
+    return g.db
+
+def close_db(e=None):
+    db = g.pop("db", None)
+
+    if db is not None:
+        # close the database 
+        pass
+
+def init_app(app):
+    app.teardown_appcontext(close_db)
+
+
+    
