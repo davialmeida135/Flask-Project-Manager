8b1d93c90617a1201f583a496dece59f7ebb4ca5
davialmeida135 <davi.almeida.135@ufrn.edu.br>
2024-06-12 23:43:00 -0300

    Post funciona

--- project_manager/app/routes/tarefas.py
+++ project_manager/app/routes/tarefas.py
@@ -1,17 +1,4 @@
 from flask import Blueprint
-
-tarefa_bp = Blueprint('tarefa', __name__)
-from marshmallow import Schema, fields
-
-class TarefaSchema(Schema):
-    nome = fields.Str(required=True)
-    data_criacao = fields.Date(required=True)
-    descricao = fields.Str(required=True)
-    prazo = fields.Date(required=True)
-    status = fields.Str(required=True)
-    idProjeto = fields.Int(required=True)
-    idUsuarios = fields.List(fields.Int(), required=True)
-
 from flask import (
     abort,
     Blueprint, 
@@ -20,13 +7,29 @@ from flask import (
     render_template, 
     url_for
 )
+from marshmallow import Schema, fields
+tarefa_bp = Blueprint('tarefa', __name__)
+
+class TarefaSchema(Schema):
+    nome = fields.Str(required=False)
+    data_criacao = fields.Date(required=False) #Será definido no codigo
+    descricao = fields.Str(required=False)
+    prazo = fields.Date(required=False)
+    status = fields.Str(required=False) #Opções seletas
+    idProjeto = fields.Int(required=False) #Será pré definido
+    idUsuarios = fields.String( required=False) #Sera definido na tela
+
+
 
 @tarefa_bp.route("/new",methods=['GET', 'POST'])
 def create_tarefa():
     if request.method == 'GET':
+        
         return render_template("create_tarefa.html")
     if request.method == 'POST':
-        
+        tarefa_schema = TarefaSchema()
+        tarefa_data = tarefa_schema.load(request.form)
+        print(tarefa_data)
         print('oi')
-        return render_template("single.html")
+        return render_template("create_tarefa.html")
         
