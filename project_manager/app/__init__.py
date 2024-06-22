727d40c607e3cbac653edb2762042026a5429608
davialmeida135 <davi.almeida.135@ufrn.edu.br>
2024-06-22 00:41:01 -0300

    Initial route defined

--- project_manager/app/__init__.py
+++ project_manager/app/__init__.py
@@ -1,4 +1,4 @@
-from flask import Flask 
+from flask import Flask, redirect, url_for 
 from flask_login import LoginManager, login_user
 from .db import init_app
 import os
@@ -15,8 +15,6 @@ def create_app():
     init_app(app)
     login_manager.init_app(app)
     
-    from .rotas import main
-    app.register_blueprint(main)
     from .routes.tarefas import tarefa_bp
     from .routes.projetos import projeto_bp
     from .routes.comentarios import comentario_bp
@@ -30,6 +28,9 @@ def create_app():
     from .routes.home import home_bp
     app.register_blueprint(home_bp, url_prefix='/home')
 
+    @app.route('/')
+    def root():
+        return redirect(url_for('auth.login'))
     return app
 
 
